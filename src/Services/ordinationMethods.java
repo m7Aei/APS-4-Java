b5de10cdcef06e774cbcce9ead9aa17602963611
m7Aei <mateuspm2015@hotmail.com>
2019-08-29 14:11:21 -0300

    Adicionando o metodo burbleSort

--- src/Services/ordinationMethods.java
+++ src/Services/ordinationMethods.java
@@ -1,4 +1,27 @@
 package Services;
 
+import Database.*;
+import Controllers.ImageController;
+
 public class ordinationMethods {
+    ImageController images = new ImageController();
+
+    // Criando o metodo burble sort
+    public void burbleSort () {
+        images.index();
+        int maximo = 0;
+        int i;
+        int tamanhoArray = images.index().size();
+        int arrayMenor = tamanhoArray - 1;
+
+        for (i = 0; i < tamanhoArray; i++) {
+            for (int y = 0; y < arrayMenor; y++) {
+                 if (arrayMenor > arrayMenor + 1) {
+                     maximo = arrayMenor;
+                     arrayMenor += 1;
+                     arrayMenor = maximo;
+                 }
+            }
+        }
+    }
 }
