d4370cc89baf3c7b5b60dfa40556bc57339b2ac1
m7Aei <mateuspm2015@hotmail.com>
2019-08-28 23:05:03 -0300

    Criando o model da imagem

---
+++ src/Models/Images.java
@@ -0,0 +1,22 @@
+package Models;
+
+public class Images {
+    private int id;
+    private byte[] image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
